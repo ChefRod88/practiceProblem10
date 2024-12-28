7cb534bb95a1f4f63e0bc16bf230b868ed7162a5
Rodney Chery <rchery7@wgu.edu>
2024-12-28 11:02:44 -0500

    Initial commit

---
+++ practiceProblem10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Dictionary representing the stock prices
+        var stocks = new Dictionary<string, double>
+        {
+            { "TSLA", 912.86 },
+            { "BBBY", 24.84 },
+            { "AAPL", 174.26 },
+            { "SOFI", 6.92 },
+            { "KIRK", 8.72 },
+            { "AURA", 22.12 },
+            { "AMZN", 141.28 },
+            { "EMBK", 12.29 },
+            { "LVLU", 2.33 }
+        };
+
+        // Accept integer input for the number of stock selections
+        var ns = int.Parse(Console.ReadLine());
+
+        double totalCost = 0;
+
+        // Loop through each stock selection and accumulate the cost
+        for (var i = 0; i < ns; i++)
+        {
+            var stockSelection = Console.ReadLine();
+            if (stocks.ContainsKey(stockSelection))
+            {
+                totalCost += stocks[stockSelection];
+            }
+            else
+            {
+                Console.WriteLine("Invalid stock selection.");
+            }
+        }
+
+        // Output the total cost of the stock selections
+        Console.WriteLine($"Total price: ${totalCost:F2}");
+    }
+}
